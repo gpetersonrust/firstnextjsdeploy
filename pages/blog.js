6a764bfdfef87e694cd12e8d85e1404db63119ae
Gino Peterson <gpeterson@rustcollege.edu>
2021-04-26 00:29:55 -0500

    practice

--- pages/blog.js
+++ pages/blog.js
@@ -10,19 +10,29 @@ function Blog({ posts }) {
   
 
   // This function gets called at build time
-export async function getStaticProps() {
-    // Call an external API endpoint to get posts
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+// export async function getStaticProps() {
+//     // Call an external API endpoint to get posts
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     const posts = await res.json()
+  
+//     // By returning { props: { posts } }, the Blog component
+//     // will receive `posts` as a prop at build time
+//     return {
+//       props: {
+//         posts,
+//       },
+//     }
+//   }
+
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
     const posts = await res.json()
   
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-      props: {
-        posts,
-      },
-    }
+    // Pass data to the page via props
+    return { props: { posts}   , revalidate: 1}
   }
+
   export default Blog
 
  
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,22 @@
+ 
 import React from 'react'
-
+ import styles from '../../styles/SinglePosts.module.css'
+ import {useRouter} from 'next/router'
 const id = ({post}) => {
 
-    console.log(post);
+
+    const router = useRouter()
+
+    // If the page is not yet generated, this will be displayed
+    // initially until getStaticProps() finishes running
+    if (router.isFallback) {
+      return <div>Loading...</div>
+    }
+  
+  
+ 
     return (
-        <div >
+        <div  className={styles.container}>
             <h2>{post.title}</h2>
         <p> {post.body}</p>
         </div>
@@ -26,7 +38,7 @@ export async function getStaticPaths() {
   
     // We'll pre-render only these paths at build time.
     // { fallback: false } means other routes should 404.
-    return { paths, fallback: false }
+    return { paths, fallback: true }
   }
 
   export async function getStaticProps({ params }) {
@@ -34,9 +46,11 @@ export async function getStaticPaths() {
     // If the route is like /posts/1, then params.id is 1
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
     const post = await res.json()
+
+    
   
     // Pass post data to the page via props
-    return { props: { post } }
+    return { props: { post }, revalidate: 1 }
   }
 
   export default id
